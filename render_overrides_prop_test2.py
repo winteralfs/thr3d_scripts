b34b3fb9336b77f12b3d63aab8e8179b85281f8a
chris winters <winteralfs@aol.com>
2019-10-24 13:57:17 -0400

    render override prop work

--- render_overrides_prop_test2.py
+++ render_overrides_prop_test2.py
@@ -66,9 +66,10 @@ class render_overrides_prop(object):
         self.light_combo_box .setMaximumWidth(180)
         self.light_combo_box .setMinimumHeight(18)
         self.light_name_layout.addWidget(self.light_combo_box)
+
         for light in self.light_names:
             self.light_combo_box.addItem(light)
-        self.light_combo_box.activated[str].connect(lambda:self.value_change())
+        self.light_combo_box.activated[str].connect(lambda:self.attribute_analysis())
 
         self.scroll = QtWidgets.QScrollArea()
         self.scroll.setWidgetResizable(False)
@@ -182,13 +183,12 @@ class render_overrides_prop(object):
         self.attribute_label = QtWidgets.QLabel('enabled')
         self.attribute_label.setFont(QtGui.QFont('SansSerif', 10))
         self.attribute_layout.addWidget(self.attribute_label)
-        enabled_checkbox = QtWidgets.QCheckBox()
-        self.attribute_layout.addWidget(enabled_checkbox)
+        self.enabled_checkbox = QtWidgets.QCheckBox()
+        self.attribute_layout.addWidget(self.enabled_checkbox)
 
         self.attribute_label = QtWidgets.QLabel('light_color  ')
         self.attribute_label.setFont(QtGui.QFont('SansSerif', 10))
         self.attribute_layout.addWidget(self.attribute_label)
-
         self.light_color_pushbutton = QtWidgets.QPushButton()
         self.light_color_pushbutton.setMinimumWidth(30)
         self.light_color_pushbutton.setMaximumWidth(30)
@@ -196,12 +196,6 @@ class render_overrides_prop(object):
         self.light_color_pushbutton.setMaximumHeight(30)
         self.light_color_pushbutton.clicked.connect(partial(self.light_color_state))
         self.attribute_layout.addWidget(self.light_color_pushbutton)
-        self.light_color_r = 100
-        self.light_color_g = 10
-        self.light_color_b = 1
-        color_string = "rgb(" + str(self.light_color_r) + "," + str(self.light_color_g) + "," + str(self.light_color_b) + ")"
-        self.light_color_pushbutton.setStyleSheet("QPushButton { background-color: %s}" %color_string)
-
 
         self.attribute_label = QtWidgets.QLabel('intensity')
         self.attribute_label.setFont(QtGui.QFont('SansSerif', 10))
@@ -214,44 +208,48 @@ class render_overrides_prop(object):
         self.attribute_intensityMult_float_spinbox.setFixedWidth(65)
         self.attribute_intensityMult_float_spinbox.setKeyboardTracking(False)
         self.attribute_layout.addWidget(self.attribute_intensityMult_float_spinbox)
+
         self.attribute_label = QtWidgets.QLabel('U size')
         self.attribute_label.setFont(QtGui.QFont('SansSerif', 10))
         self.attribute_layout.addWidget(self.attribute_label)
-        self.attribute_intensityMult_float_spinbox = custom_spin_box()
-        self.attribute_intensityMult_float_spinbox.setMinimum(-100)
-        self.attribute_intensityMult_float_spinbox.setMaximum(10000)
-        self.attribute_intensityMult_float_spinbox.setDecimals(3)
-        self.attribute_intensityMult_float_spinbox.setSingleStep(.1)
-        self.attribute_intensityMult_float_spinbox.setFixedWidth(65)
-        self.attribute_intensityMult_float_spinbox.setKeyboardTracking(False)
-        self.attribute_layout.addWidget(self.attribute_intensityMult_float_spinbox)
+        self.attribute_u_size_float_spinbox = custom_spin_box()
+        self.attribute_u_size_float_spinbox.setMinimum(-100)
+        self.attribute_u_size_float_spinbox.setMaximum(10000)
+        self.attribute_u_size_float_spinbox.setDecimals(3)
+        self.attribute_u_size_float_spinbox.setSingleStep(.1)
+        self.attribute_u_size_float_spinbox.setFixedWidth(65)
+        self.attribute_u_size_float_spinbox.setKeyboardTracking(False)
+        self.attribute_layout.addWidget(self.attribute_u_size_float_spinbox)
+
         self.attribute_label = QtWidgets.QLabel('V size')
         self.attribute_label.setFont(QtGui.QFont('SansSerif', 10))
         self.attribute_layout.addWidget(self.attribute_label)
-        self.attribute_intensityMult_float_spinbox = custom_spin_box()
-        self.attribute_intensityMult_float_spinbox.setMinimum(-100)
-        self.attribute_intensityMult_float_spinbox.setMaximum(10000)
-        self.attribute_intensityMult_float_spinbox.setDecimals(3)
-        self.attribute_intensityMult_float_spinbox.setSingleStep(.1)
-        self.attribute_intensityMult_float_spinbox.setFixedWidth(65)
-        self.attribute_intensityMult_float_spinbox.setKeyboardTracking(False)
-        self.attribute_layout.addWidget(self.attribute_intensityMult_float_spinbox)
+        self.attribute_v_size_float_spinbox = custom_spin_box()
+        self.attribute_v_size_float_spinbox.setMinimum(-100)
+        self.attribute_v_size_float_spinbox.setMaximum(10000)
+        self.attribute_v_size_float_spinbox.setDecimals(3)
+        self.attribute_v_size_float_spinbox.setSingleStep(.1)
+        self.attribute_v_size_float_spinbox.setFixedWidth(65)
+        self.attribute_v_size_float_spinbox.setKeyboardTracking(False)
+        self.attribute_layout.addWidget(self.attribute_v_size_float_spinbox)
+
         self.attribute_label = QtWidgets.QLabel('directional')
         self.attribute_label.setFont(QtGui.QFont('SansSerif', 10))
         self.attribute_layout.addWidget(self.attribute_label)
-        self.attribute_intensityMult_float_spinbox = custom_spin_box()
-        self.attribute_intensityMult_float_spinbox.setMinimum(-100)
-        self.attribute_intensityMult_float_spinbox.setMaximum(10000)
-        self.attribute_intensityMult_float_spinbox.setDecimals(3)
-        self.attribute_intensityMult_float_spinbox.setSingleStep(.1)
-        self.attribute_intensityMult_float_spinbox.setFixedWidth(65)
-        self.attribute_intensityMult_float_spinbox.setKeyboardTracking(False)
-        self.attribute_layout.addWidget(self.attribute_intensityMult_float_spinbox)
+        self.attribute_directional_float_spinbox = custom_spin_box()
+        self.attribute_directional_float_spinbox.setMinimum(-100)
+        self.attribute_directional_float_spinbox.setMaximum(10000)
+        self.attribute_directional_float_spinbox.setDecimals(3)
+        self.attribute_directional_float_spinbox.setSingleStep(.1)
+        self.attribute_directional_float_spinbox.setFixedWidth(65)
+        self.attribute_directional_float_spinbox.setKeyboardTracking(False)
+        self.attribute_layout.addWidget(self.attribute_directional_float_spinbox)
+
         self.attribute_label = QtWidgets.QLabel('use rect tex')
         self.attribute_label.setFont(QtGui.QFont('SansSerif', 10))
         self.attribute_layout.addWidget(self.attribute_label)
-        use_rect_tex_checkbox = QtWidgets.QCheckBox()
-        self.attribute_layout.addWidget(use_rect_tex_checkbox)
+        self.use_rect_tex_checkbox = QtWidgets.QCheckBox()
+        self.attribute_layout.addWidget(self.use_rect_tex_checkbox)
 
         self.attribute_label = QtWidgets.QLabel('rect_tex     ')
         self.attribute_label.setFont(QtGui.QFont('SansSerif', 10))
@@ -268,6 +266,48 @@ class render_overrides_prop(object):
         self.light_rect_color_b = 1
         color_string = "rgb(" + str(self.light_rect_color_r) + "," + str(self.light_rect_color_g) + "," + str(self.light_rect_color_b) + ")"
         self.rect_text_color_pushbutton.setStyleSheet("QPushButton { background-color: %s}" %color_string)
+
+        self.attribute_label = QtWidgets.QLabel('affect diffuse')
+        self.attribute_label.setFont(QtGui.QFont('SansSerif', 10))
+        self.attribute_layout.addWidget(self.attribute_label)
+        self.affect_diffuse_checkbox = QtWidgets.QCheckBox()
+        self.attribute_layout.addWidget(self.affect_diffuse_checkbox)
+
+        self.attribute_label = QtWidgets.QLabel('affect specular')
+        self.attribute_label.setFont(QtGui.QFont('SansSerif', 10))
+        self.attribute_layout.addWidget(self.attribute_label)
+        self.affect_specular_checkbox = QtWidgets.QCheckBox()
+        self.attribute_layout.addWidget(self.affect_specular_checkbox)
+
+        self.attribute_label = QtWidgets.QLabel('affect reflection')
+        self.attribute_label.setFont(QtGui.QFont('SansSerif', 10))
+        self.attribute_layout.addWidget(self.attribute_label)
+        self.affect_reflection_checkbox = QtWidgets.QCheckBox()
+        self.attribute_layout.addWidget(self.affect_reflection_checkbox)
+
+        self.attribute_label = QtWidgets.QLabel('diffuse contribution')
+        self.attribute_label.setFont(QtGui.QFont('SansSerif', 10))
+        self.attribute_layout.addWidget(self.attribute_label)
+        self.attribute_diffuse_contribution_float_spinbox = custom_spin_box()
+        self.attribute_diffuse_contribution_float_spinbox.setMinimum(-100)
+        self.attribute_diffuse_contribution_float_spinbox.setMaximum(10000)
+        self.attribute_diffuse_contribution_float_spinbox.setDecimals(3)
+        self.attribute_diffuse_contribution_float_spinbox.setSingleStep(.1)
+        self.attribute_diffuse_contribution_float_spinbox.setFixedWidth(65)
+        self.attribute_diffuse_contribution_float_spinbox.setKeyboardTracking(False)
+        self.attribute_layout.addWidget(self.attribute_diffuse_contribution_float_spinbox)
+
+        self.attribute_label = QtWidgets.QLabel('specular contribution')
+        self.attribute_label.setFont(QtGui.QFont('SansSerif', 10))
+        self.attribute_layout.addWidget(self.attribute_label)
+        self.attribute_specular_contribution_float_spinbox = custom_spin_box()
+        self.attribute_specular_contribution_float_spinbox.setMinimum(-100)
+        self.attribute_specular_contribution_float_spinbox.setMaximum(10000)
+        self.attribute_specular_contribution_float_spinbox.setDecimals(3)
+        self.attribute_specular_contribution_float_spinbox.setSingleStep(.1)
+        self.attribute_specular_contribution_float_spinbox.setFixedWidth(65)
+        self.attribute_specular_contribution_float_spinbox.setKeyboardTracking(False)
+        self.attribute_layout.addWidget(self.attribute_specular_contribution_float_spinbox)
 
         print self.render_layers_in_order
         self.render_layer_layout = QtWidgets.QVBoxLayout(self.main_widget)
@@ -299,15 +339,96 @@ class render_overrides_prop(object):
         self.button_horizontal_layout.addWidget(button_clear_all_overrides)
         self.button_horizontal_layout.addWidget(button_set_selected_layer_overrides)
         self.button_horizontal_layout.addWidget(button_set_all_layer_overrides)
+        self.attribute_analysis()
 
-    def value_change(self):
+    def attribute_analysis(self):
         current_light = self.light_combo_box.currentText()
         print 'current_light = ',current_light
+
+        enabled_value = cmds.getAttr(current_light + '.enabled')
+        print 'enabled_value = ',enabled_value
+        print 'setting ' + str(self.enabled_checkbox) + ' to ' + str(enabled_value)
+        self.enabled_checkbox.setChecked(enabled_value)
+
+        light_color = cmds.getAttr(current_light + '.lightColor')
+        light_color = light_color[0]
+        print 'light_color = ',light_color
+        light_color_r = light_color[0]
+        light_color_r = (light_color_r*255)
+        print 'light_color_r = ',light_color_r
+        light_color_g = light_color[1]
+        light_color_g = (light_color_g*255)
+        print 'light_color_g = ',light_color_g
+        light_color_b = light_color[2]
+        light_color_b = (light_color_b*255)
+        print 'light_color_b = ',light_color_b
+        color_string = "rgb(" + str(light_color_r) + "," + str(light_color_g) + "," + str(light_color_b) + ")"
+        self.light_color_pushbutton.setStyleSheet("QPushButton { background-color: %s}" %color_string)
+
         intensityMult_value = cmds.getAttr(current_light + '.intensityMult')
         print 'intensityMult_value = ',intensityMult_value
         print 'setting ' + str(self.attribute_intensityMult_float_spinbox) + ' to ' + str(intensityMult_value)
         self.attribute_intensityMult_float_spinbox.setValue(intensityMult_value)
 
+        u_size_value = cmds.getAttr(current_light + '.uSize')
+        print 'u_size_value = ',u_size_value
+        print 'setting ' + str(self.attribute_u_size_float_spinbox) + ' to ' + str(u_size_value)
+        self.attribute_u_size_float_spinbox.setValue(u_size_value)
+
+        v_size_value = cmds.getAttr(current_light + '.vSize')
+        print 'v_size_value = ',v_size_value
+        print 'setting ' + str(self.attribute_v_size_float_spinbox) + ' to ' + str(v_size_value)
+        self.attribute_v_size_float_spinbox.setValue(v_size_value)
+
+        directional_value = cmds.getAttr(current_light + '.directional')
+        print 'directional_value = ',directional_value
+        print 'setting ' + str(self.attribute_directional_float_spinbox) + ' to ' + str(directional_value)
+        self.attribute_directional_float_spinbox.setValue(directional_value)
+
+        use_rect_tex_value = cmds.getAttr(current_light + '.useRectTex')
+        print 'use_rect_tex_value = ',use_rect_tex_value
+        print 'setting ' + str(self.use_rect_tex_checkbox ) + ' to ' + str(use_rect_tex_value)
+        self.use_rect_tex_checkbox.setChecked(use_rect_tex_value)
+
+        rect_tex_color = cmds.getAttr(current_light + '.rectTex')
+        rect_tex_color = rect_tex_color[0]
+        print 'rect_tex_color = ',rect_tex_color
+        rect_tex_color_r = rect_tex_color[0]
+        rect_tex_color_r = (rect_tex_color_r*255)
+        print 'rect_tex_color_r = ',rect_tex_color_r
+        rect_tex_color_g = rect_tex_color[1]
+        rect_tex_color_g = (rect_tex_color_g*255)
+        print 'rect_tex_color_g = ',rect_tex_color_g
+        rect_tex_color_b = rect_tex_color[2]
+        rect_tex_color_b = (rect_tex_color_b*255)
+        print 'rect_tex_color_b = ',rect_tex_color_b
+        color_string = "rgb(" + str(rect_tex_color_r) + "," + str(rect_tex_color_g) + "," + str(rect_tex_color_b) + ")"
+        self.rect_text_color_pushbutton.setStyleSheet("QPushButton { background-color: %s}" %color_string)
+
+        affect_diffuse_value = cmds.getAttr(current_light + '.affectDiffuse')
+        print 'affect_diffuse_value = ',affect_diffuse_value
+        print 'setting ' + str(self.affect_diffuse_checkbox) + ' to ' + str(affect_diffuse_value)
+        self.affect_diffuse_checkbox.setChecked(affect_diffuse_value)
+
+        affect_specular_value = cmds.getAttr(current_light + '.affectSpecular')
+        print 'affect_specular_value = ',affect_specular_value
+        print 'setting ' + str(self.affect_specular_checkbox) + ' to ' + str(affect_specular_value)
+        self.affect_specular_checkbox.setChecked(affect_specular_value)
+
+        affect_reflection_value = cmds.getAttr(current_light + '.affectReflections')
+        print 'affect_reflection_value = ',affect_reflection_value
+        print 'setting ' + str(self.affect_reflection_checkbox) + ' to ' + str(affect_reflection_value)
+        self.affect_reflection_checkbox.setChecked(affect_reflection_value)
+
+        diffuse_contribution_value = cmds.getAttr(current_light + '.diffuseContrib')
+        print 'diffuse_contribution_value = ',diffuse_contribution_value
+        print 'setting ' + str(self.attribute_diffuse_contribution_float_spinbox) + ' to ' + str(diffuse_contribution_value)
+        self.attribute_diffuse_contribution_float_spinbox.setValue(diffuse_contribution_value)
+
+        specular_contribution_value = cmds.getAttr(current_light + '.specularContrib')
+        print 'specular_contribution_value = ',specular_contribution_value
+        print 'setting ' + str(self.attribute_specular_contribution_float_spinbox) + ' to ' + str(specular_contribution_value)
+        self.attribute_specular_contribution_float_spinbox.setValue(specular_contribution_value)
 
     def render_overrides_prop_UI(self):
         window_name = "render_overrides_prop"
@@ -320,7 +441,7 @@ class render_overrides_prop(object):
         self.window.setWindowTitle(window_name)
         #self.window.setFixedSize(1015,300)
         self.window.setFixedWidth(550)
-        self.window.setFixedHeight(625)
+        self.window.setFixedHeight(850)
         self.main_widget = QtWidgets.QWidget()
         self.window.setCentralWidget(self.main_widget)
         self.base_vertical_layout = QtWidgets.QVBoxLayout(self.main_widget)
